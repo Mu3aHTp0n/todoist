4e14bbc939d236efec8013fffcecf285d3a3e022
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-01-28 17:51:28 +0300

    board page layout change

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -3,12 +3,18 @@ import { useParams } from "react-router-dom"
 import { useBoardsStore } from "@store/BoardsStore"
 // import { useBoardsStore } from "../../store/BoardsStore"
 
+import styles from './BoardPage.module.css'
+
 export default function BoardPage() {
   const { name } = useParams();
   const [list, setList] = useState([])
   const boards = useBoardsStore(state => state.boards);
-
   let listLength = 0;
+
+  const allLists = list?.map(list => {
+    return <p>Компонент листа</p>
+  })
+
 
 	useEffect(() => {
 		if(!name) return
@@ -18,10 +24,10 @@ export default function BoardPage() {
 	},[name])
 
   return (
-    <div>
-      <p>Доска: <span>{name}</span></p>
-      <p>Количество списков: <span>{listLength}</span></p>
-      <p>Списки: <span>{list?.map(item => {return item})}</span></p>
+    <div className={styles.container}>
+      <h3>Доска: {name}</h3>
+      { allLists }
+      {/* Форма для добавления нового листа */}
     </div>
   )
 }
