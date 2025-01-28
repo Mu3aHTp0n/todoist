49e200ba92a49a1c128994e976ca5abfe9fac3db
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-01-28 17:33:00 +0300

    feature: layout board detail page

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { useBoardsStore } from "@store/BoardsStore"
+// import { useBoardsStore } from "../../store/BoardsStore"
 
 export default function BoardPage() {
-  const { name } = useParams()
+  const { name } = useParams();
+  const [list, setList] = useState([])
+  const boards = useBoardsStore(state => state.boards);
+
+  let listLength = 0;
 
 	useEffect(() => {
 		if(!name) return
-		
+    const currentBoard = boards.filter(board => board.title === name);
+    setList(currentBoard.list)
+    listLength = list.length
 	},[name])
 
   return (
-    <div>BoardPage {name}</div>
+    <div>
+      <p>Доска: <span>{name}</span></p>
+      <p>Количество списков: <span>{listLength}</span></p>
+      <p>Списки: <span>{list?.map(item => {return item})}</span></p>
+    </div>
   )
 }
