26d30ada9aacc972fa6ee162b7e93801c15b8293
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-02-06 06:50:12 +0300

    refactor: generating id with uuid library

--- src/store/BoardsStore.ts
+++ src/store/BoardsStore.ts
@@ -1,51 +1,77 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 import { IBoardStore } from './type';
 
 export const useBoardsStore = create<IBoardStore>()(
-	persist(
-		set => ({
-			boards: [],
-			addBoard: (name) => {
-				set(state => {
-					const newId = state.boards.length > 0 ? Math.max(...state.boards.map(board => board.id)) + 1 : 0;
-					return {
-						boards: [...state.boards, {
-							id: newId,
-							title: name,
-							list: []
-						}]
-					}
-				})
-			},
-			removeBoard: (id) =>
-				set(state => ({
-					boards: state.boards.filter(board => board.id !== id)
-				})),
-				addList: (boardId, listName) => {
-					set((state) => {
-						const boardIndex = state.boards.findIndex(board => board.id === boardId);
-			
-						const newListId = state.boards[boardIndex].list.length > 0 
-							? Math.max(...state.boards[boardIndex].list.map(list => list.id)) + 1 
-							: 0;
-			
-						const newList = {
-							id: newListId,
-							title: listName,
-							listItem: []
-						};
-			
-						const updatedBoards = [...state.boards];
-						updatedBoards[boardIndex].list.push(newList);
-			
-						return { boards: updatedBoards };
-					});
-				},
-		}),
-		{
-			name: 'boards-storage',
-		}
-	)
-)
+  persist(
+    set => ({
+      boards: [],
+      addBoard: name => {
+        set(state => {
+          return {
+            boards: [
+              ...state.boards,
+              {
+                id: uuidv4(),
+                title: name,
+                list: [],
+              },
+            ],
+          };
+        });
+      },
+      removeBoard: id =>
+        set(state => ({
+          boards: state.boards.filter(board => board.id !== id),
+        })),
+      addList: (boardId, listName) => {
+        set(state => {
+          const boardIndex = state.boards.findIndex(
+            board => board.id === boardId,
+          );
+
+          const newList = {
+            id: uuidv4(),
+            title: listName,
+            listItem: [],
+          };
+
+          const updatedBoards = [...state.boards];
+          updatedBoards[boardIndex].list.push(newList);
+
+          return { boards: updatedBoards };
+        });
+      },
+      addListItem: (boardId, listId, itemName) => {
+        set(state => {
+          const boardIndex = state.boards.findIndex(
+            board => board.id === boardId,
+          );
+          console.log('board of index: ', state.boards[boardIndex]);
+          const listIndex = state.boards[boardIndex].list.findIndex(
+            list => list.id === listId,
+          );
+
+          console.warn('listIndex: ', listIndex);
+          console.warn(state.boards[boardIndex].list[listIndex]);
+
+          const newListItem = {
+            id: uuidv4(),
+            description: itemName,
+            status: false,
+          };
+
+          const updatedBoards = [...state.boards];
+          updatedBoards[boardIndex].list[listIndex].listItem.push(newListItem);
+          return { boards: updatedBoards };
+        });
+      },
+    }),
+    {
+      name: 'boards-storage',
+    },
+  ),
+);
--- src/store/type.ts
+++ src/store/type.ts
@@ -1,24 +1,25 @@
 export interface IBoardStore {
-    boards: IBoard[];
-    addBoard: (boardName: string) => void;
-    removeBoard: (boardId: number) => void;
-    addList: (boardId: number, listName: string) => void;
+  boards: IBoard[];
+  addBoard: (boardName: string) => void;
+  removeBoard: (boardId: string) => void;
+  addList: (boardId: string, listName: string) => void;
+  addListItem: (boardId: string, listId: string, itemTitle: string) => void;
 }
 
 export interface IBoard {
-    id: number,
-    title: string,
-    list: IList[]
+  id: string;
+  title: string;
+  list: IList[];
 }
 
 export interface IList {
-    id: number,
-    title: string,
-    listItem: IListItem[];
+  id: string;
+  title: string;
+  listItem: IListItem[];
 }
 
 export interface IListItem {
-    id: number,
-    description: string,
-    status: boolean,
+  id: string;
+  description: string;
+  status: boolean;
 }
