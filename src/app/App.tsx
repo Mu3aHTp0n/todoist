0377f1bc682b308fb4b386b447e6a1940f664329
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-02-21 02:43:49 +0300

    refactor: access for token

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from 'react';
 
-import BoardsPage from "@pages/BoardsPage/BoardsPage"
-import BoardPage from "@pages/BoardPage/BoardPage"
-import Header from "@widgets/Header/Header"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import BoardsPage from '@pages/BoardsPage/BoardsPage';
+import BoardPage from '@pages/BoardPage/BoardPage';
+import Page401 from '@pages/Page401/Page401';
+import Header from '@widgets/Header/Header';
+import AuthForm from '@widgets/AuthForm/AuthForm';
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    setIsLogged(localStorage.getItem('accessToken') ? true : false);
+  }, []);
+
   return (
     <BrowserRouter>
-      <Header />
+      {isLogged ? <Header /> : ''}
       <Routes>
-        <Route path="/boards" element={<BoardsPage /> } />
-        <Route path="/boards/:id" element={<BoardPage /> } />
+        <Route
+          path='/'
+          element={isLogged ? <Navigate to='/boards' replace /> : <AuthForm />}
+        />
+        <Route
+          path='/boards'
+          element={isLogged ? <BoardsPage /> : <Page401 />}
+        />
+        <Route
+          path='/boards/:id'
+          element={isLogged ? <BoardPage /> : <Page401 />}
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
