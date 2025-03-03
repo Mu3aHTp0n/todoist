43a4254644336f76a3b0b749754d552a35dc82fb
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-03-04 00:36:00 +0300

    .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +12,6 @@ export default defineConfig({
       '@store': path.resolve(__dirname, './src/store'),
       '@widgets': path.resolve(__dirname, './src/widgets'),
       '@shared': path.resolve(__dirname, './src/shared'),
-    }
-  }
-})
+    },
+  },
+});
