2564ac5c8d64d5597fd01df55e0520bcaab906fd
Mu3aHTp0n <kovalevartur05@gmail.com>
2025-03-04 10:47:05 +0300

    refactor: route provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,11 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
-import BoardsPage from '@pages/BoardsPage/BoardsPage';
-import BoardPage from '@pages/BoardPage/BoardPage';
-import Page401 from '@pages/Page401/Page401';
-import Header from '@widgets/Header/Header';
-import AuthForm from '@widgets/AuthForm/AuthForm';
+import RouterProvider from './providers/RouterProvider';
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
-  useEffect(() => {
-    setIsLogged(localStorage.getItem('accessToken') ? true : false);
-  }, []);
-
   return (
     <BrowserRouter>
-      {isLogged ? <Header /> : ''}
-      <Routes>
-        <Route
-          path='/'
-          element={isLogged ? <Navigate to='/boards' replace /> : <AuthForm />}
-        />
-        <Route
-          path='/boards'
-          element={isLogged ? <BoardsPage /> : <Page401 />}
-        />
-        <Route
-          path='/boards/:id'
-          element={isLogged ? <BoardPage /> : <Page401 />}
-        />
-      </Routes>
+      <RouterProvider />
     </BrowserRouter>
   );
 }
